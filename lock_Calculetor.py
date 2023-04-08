ed2fd6fc34dcae131a35227d848232fcc4c3af97
Virtual690 <130254751+Virtual690@users.noreply.github.com>
2023-04-09 04:21:11 +0600

    Add files via upload

---
+++ lock_Calculetor.py
@@ -0,0 +1,58 @@
+password = 'B.S.A'
+p = input('password : ')
+
+if p == password :
+    print()
+
+    print('๑۞๑,¸¸,ø¤º°`°๑۩   🎀  CALCULATOR  🎀   ۩๑°`°º¤ø,¸¸,๑۞๑')
+    print('˜”*°•.˜”*°• Made By The B.S.A Group •°*”˜.•°*”˜')
+
+    print()
+
+    while True:   # for starting the infinity loop
+
+        a = int(input('1st Number : '))
+        s = input("'+','-','*','/' Here : ")
+
+        if s.lower() == 'q':  # for exit the function [ varial name . lower() ]
+            print('Exiting...')
+            break  # for take a rest . and aging continue
+
+        b = int(input('2nd Number : '))
+
+        if s == '+':
+            print()
+            print('𝓐𝓷𝓼𝔀𝓮𝓻 : ')
+            print(a + b)
+            print()
+
+
+
+        elif s == '-':
+            print()
+            print('𝓐𝓷𝓼𝔀𝓮𝓻 : ')
+            print(a - b)
+            print()
+
+
+        elif s == '*':
+            print()
+            print('𝓐𝓷𝓼𝔀𝓮𝓻 : ')
+            print(a * b)
+            print()
+
+
+        elif s == '/':
+            print()
+            print('𝓐𝓷𝓼𝔀𝓮𝓻 : ')
+            print(a / b)
+            print()
+
+
+        else:
+            print(' ---🅴🆁🅾🆁🆁 - --')
+
+            print()
+            print()
+else :
+    print('Worng')
